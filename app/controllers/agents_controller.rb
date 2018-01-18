460fd026868090ff7ff82c4f200ec74aa4c4109e
zekebergida <zekebergida@gmail.com>
2018-01-17 23:24:33 -0500

    fix agents controler

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -3,35 +3,5 @@ class AgentsController < ApplicationController
     @agents = Agent.all
   end
 
-  def show
-    @agent = Agent.find(params[:id])
-    render 'show.json.jbuilder'
-  end
-
-  def create
-    
-    @agent = Agent.create(
-      field_name: params[:field_name],
-      base: params[:base],
-      deployment_date: params[:deployment_date]
-    )
-    render 'show.json.jbuilder'
-  end
-
-  def update
-    @agent = Agent.find(params[:id])
-    @agent.update(
-      field_name: params[:field_name],
-      base: params[:base],
-      deployment_date: params[:deployment_date]
-    )
-    render 'show.json.jbuilder'
-  end
-
-  def destroy
-    @agents = Agent.all
-    @agent = Agent.find(params[:id])
-    @agent.destroy
-    render 'index.json.jbuilder'
-  end
+  
 end
